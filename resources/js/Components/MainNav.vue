41bab24dba3431c2d79cb75f6341cda1ca406eb3
FRANZSAIJEM <mikoremixes@gmail.com>
2023-09-14 19:24:28 +0800

    first

---
+++ resources/js/Components/MainNav.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { defineProps } from 'vue';
+
+const {hidden} = defineProps (['hidden'])
+</script>
+
+<template>
+
+    <div class="main-nav" style="text-align: start;">
+        <Link :href="route('dashboard')" :active="route().current('dashboard')">
+            <i style="width: 45px; margin-left: -13px; background-color: rgb(32, 106, 32); text-align: center; box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.159); padding: 10px; border-radius: 5px;" class="fa-solid fa-house-chimney"></i> <span :hidden="hidden">Dashboard</span>
+        </Link>
+        <Link :href="route('clients')" :active="route().current('clients')">
+            <i style="width: 45px; margin-left: -13px; background-color: rgb(32, 106, 32); text-align: center; box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.159); padding: 10px; border-radius: 5px;" class="fa-solid fa-users"></i> <span :hidden="hidden">Clients</span>
+        </Link>
+        <Link :href="route('profile.edit')" :active="route().current('clients')">
+            <i style="width: 45px; margin-left: -13px; background-color: rgb(32, 106, 32); text-align: center; box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.159); padding: 10px; border-radius: 5px;" class="fa-solid fa-user"></i> <span :hidden="hidden">Profile</span>
+        </Link>
+        <Link :href="route('logout')" method="post" >
+            <i style="width: 45px; margin-left: -13px; background-color: rgb(32, 106, 32); text-align: center; box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.159); padding: 10px; border-radius: 5px;" class="fa-solid fa-right-from-bracket"></i> <span :hidden="hidden">Logout</span>
+        </Link>
+
+    </div>
+
+</template>
---
+++ resources/js/Pages/Clients.vue
@@ -0,0 +1,78 @@
+<script setup>
+import Layout from '@/Layouts/Layout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+
+<template>
+    <Head title="Clients" />
+
+    <Layout>
+        <div class="">
+            <div class="">
+                <div class="">
+                    <div class="p-6 text-gray-900">
+                        <div v-for="client in clients.data" :key="client.id" style="display: inline-flex; flex-wrap: wrap;">
+                            <div class="bg-green-900 text-white m-5 p-5 rounded-lg" style="width: 350px;">
+                               <div class="rounded-2xl shadow-2xl mb-5" style="display: grid; place-content: center; background-color: rgb(46, 131, 46);">
+                                <img width="170" src="https://static.vecteezy.com/system/resources/previews/022/665/468/non_2x/excited-3d-boy-jumping-on-white-transparent-background-free-png.png" alt="">
+                               </div>
+                                <b>Last Name: </b>{{ client.last_name }} <br>
+                                <b>First Name: </b>{{ client.first_name }} <br>
+                                <b>Address: </b>{{ client.address }} <br>
+                                <b>Phone: </b>{{ client.phone }} <br>
+                                <b>Level: </b>{{ client.level }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-4 flex justify-between items-center">
+    <div class="flex items-center space-x-2" style="transform: translateY(-20px) translateX(45px);">
+        <span v-if="clients.prev_page_url">
+            <a
+                :href="clients.prev_page_url"
+                @click.prevent="$inertia.visit(clients.prev_page_url)"
+                class="text-sm font-medium text-gray-100 p-5 bg-green-900 rounded-md hover:bg-green-700 duration-100"
+            >
+                Previous
+            </a>
+        </span>
+
+        <!-- Numbered Links Pagination -->
+        <template v-if="clients.last_page > 1">
+            <span v-for="page in clients.last_page" :key="page">
+                <a
+                    :href="'/clients?page=' + page"
+                    @click.prevent="$inertia.visit('/clients?page=' + page)"
+                    class="text-sm font-medium text-gray-100 p-5 bg-green-800 rounded-md hover:bg-green-700 duration-100"
+                >
+                    {{ page }}
+                </a>
+            </span>
+        </template>
+
+        <span v-if="clients.next_page_url">
+            <a
+                :href="clients.next_page_url"
+                @click.prevent="$inertia.visit(clients.next_page_url)"
+                class="text-sm font-medium text-gray-100 p-5 bg-green-900 rounded-md hover:bg-green-700 duration-100"
+            >
+                Next
+            </a>
+        </span>
+    </div>
+</div>
+
+    </Layout>
+</template>
+
+<script>
+export default {
+    props: {
+        clients: Object, // Define the clients prop type as an object
+    },
+};
+</script>
